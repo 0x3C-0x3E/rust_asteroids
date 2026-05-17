5843b5bbe093f6b4f865a6e9d814212c97a08e5e
0x3C-0x3E <rbng0094@gmail.com>
2026-05-17 16:49:51 +0200

    update

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,32 @@
-use macroquad::prelude::*;
+use macroquad::{miniquad::window::set_window_position, prelude::*};
 
+#[derive(Debug, Default)]
 struct Player {
     x: i32,
     y: i32,
+
+    x_vel: f32,
+    y_vel: f32,
 }
 
 impl Player {
     fn new(x: i32, y: i32) -> Self {
-        Self { x, y }
+        Self {
+            x,
+            y,
+            ..Default::default()
+        }
     }
 
-    fn update() {}
+    fn draw(&self) {
+        draw_circle(self.x as f32, self.y as f32, 10.0, RED);
+    }
+
+    fn update(&mut self) {
+        if is_key_down(KeyCode::W) {
+            self.y_vel = -5.0;
+        }
+    }
 }
 
 struct GameState {
@@ -18,16 +34,44 @@ struct GameState {
 }
 
 impl GameState {
-    fn new(player: Player) -> Self {
-        Self { player }
+    fn new() -> Self {
+        Self {
+            player: Player::new(
+                (screen_width() / 2.0) as i32,
+                (screen_height() / 2.0) as i32,
+            ),
+        }
+    }
+
+    fn draw(&self) {
+        self.player.draw();
+    }
+
+    fn update(&mut self) {
+        self.player.update();
     }
 }
 
-#[macroquad::main("Asteroids")]
+fn window_conf() -> Conf {
+    Conf {
+        window_title: "Asteroids!".to_owned(),
+        window_width: 400,
+        window_height: 400,
+        window_resizable: false,
+        ..Default::default()
+    }
+}
+
+#[macroquad::main(window_conf())]
 async fn main() {
+    let game = GameState::new();
+
+    set_window_position(1920 / 2 - 200, 1080 / 2 - 200);
+
     loop {
         clear_background(BLACK);
 
+        game.draw();
         next_frame().await
     }
 }
